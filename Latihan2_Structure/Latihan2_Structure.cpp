64616f877d9137ef1be59ad2abef7c89b57c5c11
nrismawardah <nrismawardah@gmail.com>
2023-04-03 10:55:36 +0700

    Menampilkan baris mahasiswa

--- Latihan2_Structure/Latihan2_Structure.cpp
+++ Latihan2_Structure/Latihan2_Structure.cpp
@@ -31,4 +31,9 @@ int main() {
 	cout << "Masukkan Umur; ";
 	cin >> mhs.umur;
 
+	cout << "\n\nNIM = " << mhs.NIM;
+	cout << "\nNama = " << mhs.nama;
+	cout << "\nDesa = " << mhs.alamat.desa;
+	cout << "\nKota = " << mhs.alamat.kota;
+	cout << "\nUmur = " << mhs.umur;
 }

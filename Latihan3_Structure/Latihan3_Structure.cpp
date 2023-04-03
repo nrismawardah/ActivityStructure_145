8288723dc8d847ceb6f2a73da05e815768765887
nrismawardah <nrismawardah@gmail.com>
2023-04-03 11:21:34 +0700

    Menampilkan baris mahasiswa

--- Latihan3_Structure/Latihan3_Structure.cpp
+++ Latihan3_Structure/Latihan3_Structure.cpp
@@ -14,3 +14,32 @@ struct Mahasiswa
 	AlamatDetail alamat;
 	int umur;
 };
+
+
+int main() {
+	Mahasiswa mhs[3];
+
+	for (int i = 0; i < 3; i++) {
+		cout << "Masukkan NIM :";
+		cin.getline(mhs[i].NIM, 12);
+		cout << "Masukkan Nama :";
+		cin.getline(mhs[i].nama, 25);
+		cout << "Alamat :";
+		cout << "\n\tMasukkan Desa :";
+		cin.getline(mhs[i].alamat.kota, 20);
+		cout << "\tMasukkan Kota :";
+		cin.getline(mhs[i].alamat.kota, 20);
+		cout << "Masukkan Umur :";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "\n\nNIM = " << mhs[i].NIM;
+		cout << "\nNama = " << mhs[i].nama;
+		cout << "\nDesa = " << mhs[i].alamat.desa;
+		cout << "\nKota = " << mhs[i].alamat.kota;
+		cout << "\nUmur = " << mhs[i].umur;
+	}
+}

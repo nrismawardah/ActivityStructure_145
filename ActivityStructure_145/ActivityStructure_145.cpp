e73799a032f33d5c5c6fe0a268a26e0812c12525
nrismawardah <nrismawardah@gmail.com>
2023-04-03 10:27:23 +0700

    Menampilkan display mhs2

--- ActivityStructure_145/ActivityStructure_145.cpp
+++ ActivityStructure_145/ActivityStructure_145.cpp
@@ -17,6 +17,16 @@ int main() {
     mhs1.nama = "Ardian Mulya";
     mhs1.alamat = "Wonosobo";
     mhs1.umur = 20;
+
+    cout << "Masukkan NIM :";
+    cin >> mhs2.NIM;
+    cout << "Masukkan Nama :";
+    cin >> mhs2.nama;
+    cout << "Masukkan Alamat :";
+    cin >> mhs2.alamat;
+    cout << "Masukkan Umur :";
+    cin >> mhs2.umur;
+
 }
 
 int main()

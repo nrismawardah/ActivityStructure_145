f2e39914991163275427b8fb7462c52b328bc60f
nrismawardah <nrismawardah@gmail.com>
2023-04-03 10:15:49 +0700

    Memasukkan data mhs1

--- ActivityStructure_145/ActivityStructure_145.cpp
+++ ActivityStructure_145/ActivityStructure_145.cpp
@@ -9,6 +9,16 @@ struct mahasiswa
     int umur;
 };
 
+
+int main() {
+    mahasiswa mhs1, mhs2;
+
+    mhs1.NIM = "20210140019";
+    mhs1.nama = "Ardian Mulya";
+    mhs1.alamat = "Wonosobo";
+    mhs1.umur = 20;
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
